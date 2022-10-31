331d540a4d8d78a4e8c6f5c2ba588a27ddbc0ff2
KEV_home <e.u.kuznetsov@yandex.ru>
2022-11-01 00:41:29 +0300

    refactor\ main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import ru.yandex.practicum.managers.HistoryManager;
 import ru.yandex.practicum.managers.Managers;
+import ru.yandex.practicum.managers.TaskManager;
 import ru.yandex.practicum.tasks.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        TaskManager tm = Managers.getDefault();
+        HistoryManager hm = Managers.getDefaultHistory();
 
         Task task = new Task("Таск", "-");
         Task task1 = new Task("Таск", "-");
@@ -10,11 +15,11 @@ public class Main {
         Task task3 = new Task("Таск", "-");
         Task task4 = new Task("Таск", "-");
 
-        Managers.getDefault().create(task);
-        Managers.getDefault().create(task1);
-        Managers.getDefault().create(task2);
-        Managers.getDefault().create(task3);
-        Managers.getDefault().create(task4);
+        tm.create(task);
+        tm.create(task1);
+        tm.create(task2);
+        tm.create(task3);
+        tm.create(task4);
 
         Epic epic = new Epic("Эпик", "-");
         Epic epic1 = new Epic("Эпик", "-");
@@ -22,12 +27,12 @@ public class Main {
         Epic epic3 = new Epic("Эпик", "-");
         Epic epic4 = new Epic("Эпик", "-");
 
-        Managers.getDefault().create(epic);
-        Managers.getDefault().create(epic1);
-        Managers.getDefault().create(epic2);
-        Managers.getDefault().create(epic3);
-        Managers.getDefault().create(epic4);
-        Managers.getDefault().create(epic4);
+        tm.create(epic);
+        tm.create(epic1);
+        tm.create(epic2);
+        tm.create(epic3);
+        tm.create(epic4);
+        tm.create(epic4);
 
         SubTask subTask = new SubTask("Сабтаск", "-", epic.getID());
         SubTask subTask1 = new SubTask("Сабтаск", "-", epic1.getID());
@@ -35,16 +40,25 @@ public class Main {
         SubTask subTask3 = new SubTask("Сабтаск", "-", epic3.getID());
         SubTask subTask4 = new SubTask("Сабтаск", "-", epic.getID());
 
-        Managers.getDefault().create(subTask);
-        Managers.getDefault().create(subTask1);
-        Managers.getDefault().create(subTask2);
-        Managers.getDefault().create(subTask3);
-        Managers.getDefault().create(subTask4);
+        tm.create(subTask);
+        tm.create(subTask1);
+        tm.create(subTask2);
+        tm.create(subTask3);
+        tm.create(subTask4);
 
-        Managers.getDefault().getTaskByID(task3.getID());
-        Managers.getDefault().getSubTaskByID(subTask2.getID());
-        Managers.getDefault().getSubTaskByID(subTask2.getID());
+        tm.getTaskByID(task3.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getEpicByID(epic1.getID());
+        tm.getEpicByID(epic1.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getTaskByID(task4.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getSubTaskByID(subTask2.getID());
+        tm.getTaskByID(task3.getID());
 
-        System.out.println(Managers.getDefaultHistory().getHistory().toString());
+        System.out.println(hm.getHistory().toString());
     }
 }
